0944f20319f87c3e9bea456e9775b0735860e9cb
limjeein <jeein777@naver.com>
2025-04-08 19:51:23 +0900

    안전한 버전 수정

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,39 @@ const nextConfig = {
     return config
   },
   transpilePackages: ['react-leaflet'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    minimumCacheTTL: 60,
+  },
+  poweredByHeader: false,
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin',
+        }
+      ],
+    },
+  ],
 }
 
 export default nextConfig
